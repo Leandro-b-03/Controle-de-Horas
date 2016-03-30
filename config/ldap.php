b35aa088e3982ffb4bcd2556c903e66b02d5ad7a
Leandro-b-03 <leandro.b.03@gmail.com>
2016-03-29 22:06:21 -0300

    Update ldap.php

--- config/ldap.php
+++ config/ldap.php
@@ -3,11 +3,11 @@
 return [
     'plugins' => [
         'adldap' => [
-            'account_suffix'     => '@someideias.local',
+            'account_suffix'     => '@svlabs.local',
             'domain_controllers' => [
-                '192.168.80.128'
+                'dc2.svlabs.local'
             ], // Load balancing domain controllers
-            'base_dn' => 'DC=someideias,DC=local',
+            'base_dn' => 'DC=svlabs,DC=local',
             'admin_username' => 'timesheet', // 'timesheet', // This is required for session persistance in the application
             'admin_password' => 'NGxJrOVf9DwT9VMJYix1', // '7DFWT)EVu6',
             'use_ssl' => false, // If TLS is true this MUST be false.
