ca9379b0ab8fbe5a1fc56c6b3e7e7741a8f513cb
Leandro Bezerra <lbezerra@svlabs.local>
2016-09-02 10:03:03 -0300

    Dashboard fix

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('title')
-    {!! Lang::get('general.app-tittle', ['controller' => Lang::get('errors.503')]) !!}
+    {!! Lang::get('general.app-tittle', ['controller' => Lang::get('errors.500')]) !!}
 @stop
 
 @section('style')
@@ -9,7 +9,7 @@
 
 @section('content')
           <h1>
-            {!! Lang::get('errors.503') !!}
+            {!! Lang::get('errors.500') !!}
           </h1>
         </section>
 
@@ -17,11 +17,11 @@
         <section class="content">
 
           <div class="error-page">
-            <h2 class="headline text-orange">{!! Lang::get('errors.503-g') !!}</h2>
+            <h2 class="headline text-orange">{!! Lang::get('errors.500-g') !!}</h2>
             <div class="error-content">
-              <h3><i class="fa fa-warning text-orange"></i> {!! Lang::get('errors.503-description') !!}</h3>
+              <h3><i class="fa fa-warning text-orange"></i> {!! Lang::get('errors.500-description') !!}</h3>
               <p>
-                {!! Lang::get('503-errors.info') !!}
+                {!! Lang::get('errors.500-info') !!}
               </p>
             </div>
           </div><!-- /.error-page -->
